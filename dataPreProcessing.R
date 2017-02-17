0776de773313dd774704f92068b6e5ba29f14f56
afonsofvaz <afonsofvaz@gmail.com>
2017-02-17 09:34:42 -0200

    initial

--- dataPreProcessing.R
+++ dataPreProcessing.R
@@ -1,6 +1,7 @@
 require(readxl)
-dir="/home/afonso/Small/Dengue"
-setwd(dir)
+
+setwd(system("pwd", intern = T) )
+
 read_excel_allsheets <- function(filename,removeSheet=NULL,s=0,cond=TRUE) {
   sheets <- readxl::excel_sheets(filename)
   sheets = sheets[-removeSheet]
@@ -10,16 +11,11 @@ read_excel_allsheets <- function(filename,removeSheet=NULL,s=0,cond=TRUE) {
   names(x) <- sheets
   x
 }
-mysheets <- read_excel_allsheets("data.xlsx",removeSheet = 1,s=5)
-mysheets
-names(mysheets)
 
-regions=mysheets[[1]][,1][1:33]
-regions
+mysheets <- read_excel_allsheets("data.xlsx",removeSheet=1,s=5)
 
 for(i in 1:length(names(mysheets))){
-  mysheets[[i]]=mysheets[[i]][,!is.na(names(mysheets[[1]]))]
+  mysheets[[i]]=mysheets[[i]][,!is.na(names(mysheets[[i]]))]
   mysheets[[i]]=subset(mysheets[[i]],select=-c(TOTAL))
   mysheets[[i]]=mysheets[[i]][1:33,]
 }
-mysheets[[1]][1]
